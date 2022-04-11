fa79b5dbe2f471d0b2be201a90dc918b1d56f0c5
rivmos <rivmos.official@gmail.com>
2022-04-11 17:13:52 +0530

    alternative to jquery given

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// let back = document.getElementById('bannerImg');
+ // let back = document.getElementById('bannerImg');
 // let item = document.querySelectorAll('.hamsa');
 
 // item.addEventListener('mouseover',function(){
@@ -12,11 +12,27 @@
 
 
 let back = document.getElementById('bannerImg');
-$('.hamsa').mouseover(function () {
-    back.style.filter = 'blur(3px)';
-    back.style.transition = '0.8s';
-});
-$('.hamsa').mouseleave(function () {
+// $('.hamsa').mouseover(function () {
+//     back.style.filter = 'blur(3px)';
+//     back.style.transition = '0.8s';
+// });
+// $('.hamsa').mouseleave(function () {
+//     back.style.filter = 'blur(0.5px)';
+//     back.style.transition = '0.8s';
+// });
+
+let imgs = document.getElementsByClassName('hamsa');
+Array.from(imgs).forEach(function(element){
+    element.addEventListener('mouseover',on_func);
+    element.addEventListener('mouseleave',off_func);
+})
+
+function on_func() {
+        back.style.filter = 'blur(3px)';
+        back.style.transition = '0.8s';
+    }
+
+function off_func() {
     back.style.filter = 'blur(0.5px)';
     back.style.transition = '0.8s';
-});
+}
